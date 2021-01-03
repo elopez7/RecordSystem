e741bb27cbcfe89149caa36a4f6c9a7735a5de6d
Esteban Lopez <e.lopez7@hotmail.com>
2021-01-03 14:52:33 -0500

    Add project files.

---
+++ src/RecordSystem.cpp
@@ -0,0 +1,12 @@
+﻿// RecordSystem.cpp : Defines the entry point for the application.
+//
+
+#include "RecordSystem.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
